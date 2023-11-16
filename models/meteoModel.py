2b7da01113677e5aaefd7bd26ff4a2f21cf44d1d
Maxime-Quenault <77842815+Maxime-Quenault@users.noreply.github.com>
2023-11-16 18:06:33 +0100

    bou

--- models/meteoModel.py
+++ models/meteoModel.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import List
 from datetime import date
 from departementModel import Departement
-from  infometeoModel import InfoMeteo
+from infometeoModel import InfoMeteo
 
 class Meteo(BaseModel):
     departement: Departement
